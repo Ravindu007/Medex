16d8d1dab25e42d1de9351763a0b03b48950b971
Ravindu007 <ravindu0504@gmail.com>
2022-07-12 12:57:22 +0530

    buttons

--- src/components/Departments/Departments.js
+++ src/components/Departments/Departments.js
@@ -8,22 +8,28 @@ import xRayImage from "../../images/xray.jpg"
 import { useState } from 'react'
 
 const Departments = () => {
-  const [buttons, setButtons] = useState([
-    {name:"Neurology", id:1}, 
-    {name:"Ophthalmology", id:2},
-    {name:"Nuclear Magnetic", id:3},
-    {name:"Surgical", id:4},
-    {name:"X Ray", id:5},
-    {name:"Cardiology", id:6},
-    {name:"Dental Clinic", id:7}
+  const [buttons] = useState([
+    {name:"Neurology", data:"This is Neurology",  id:1}, 
+    {name:"Ophthalmology", data: "This is Ophthalmology", id:2},
+    {name:"Nuclear Magnetic",data:"This is Nuclear Magnetic", id:3},
+    {name:"Surgical",data:"This is Surgical", id:4},
+    {name:"X Ray",data:"This is X Ray", id:5},
+    {name:"Cardiology",data:"This is Cardiology", id:6},
+    {name:"Dental Clinic",data:"This is Dental Clinic", id:7}
   ])
+
+  const setData = (button) =>{
+     return button.data
+  }
+
+
 
   return (
     <div className="departments">
       <div className="row">
         <div className="col-md-3 list">
           {buttons.map((button)=>(
-              <button key={button.id}>
+              <button key={button.id} onClick={setData.bind(this, button)}>
                 <FontAwesomeIcon icon={faNotesMedical} style={{marginRight:"10px"}}/>
                 {button.name}
               </button>
@@ -33,9 +39,8 @@ const Departments = () => {
           <img src={xRayImage} alt=""  className='d-block img-fluid'/>
         </div>
         <div className="col-md-5 desc">
-          <h5>Neurology</h5>
-          <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.
-          The Big Oxmox advised her not to do so, because there were thousands of bad Commas, wild Question Marks and devious Semikoli, but the Little Blind Text didn’t listen. She packed her seven versalia, put her initial into the belt and made herself on the way.</p>
+          <h5></h5>
+          <p></p>
           <div className="details">
             <p className="detail-line">The Big Oxmox advised her not to do so</p>
             <p className="detail-line">Far far away, behind the word mountains</p>
